d18604a5451bcfb926c16f7cc003ac4f5512bb9f
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-10 10:05:35 -0800

    added: First API

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const stat = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    }
+    res.status(200).json(stat);
+  }
+}
+
+module.exports = AppController;
