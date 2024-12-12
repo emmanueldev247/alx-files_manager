0238b05fc34ffd84f7969b6319e48a77ff853a89
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-12 03:32:29 -0800

    added: Create a new user

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || '5000';
 
+app.use(express.json());
 app.use('/', routes);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
