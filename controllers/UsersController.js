38584815728526a2dd473dd3b75a8e7888c569ba
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-13 06:51:40 -0800

    ensured: DB Connection Before API Call

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -35,6 +35,7 @@ class UsersController {
       const userId = await redisClient.get(key);
       if (!userId) return res.status(401).json({ error: 'Unauthorized' });
 
+      if (!dbClient.DB) await dbClient.init();
       const usersCollection = dbClient.DB.collection('users');
       const user = await usersCollection.findOne({ _id: ObjectId(userId) });
 
