5bd3cea3a06a4769817e46eb04e8ee2de2f50995
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-13 07:26:40 -0800

    fixed: POST /files creates a folder at the root

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -50,14 +50,18 @@ class FilesController {
         fileDocument.id = result.insertedId;
       } else {
         const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
-        await fs.mkdir(FOLDER_PATH, { recursive: true });
-        const localPath = path.join(FOLDER_PATH, uuidv4());
+        const parentFolderPath = parentId === '0'
+          ? FOLDER_PATH
+          : path.join(FOLDER_PATH, parentId);
+        await fs.mkdir(parentFolderPath, { recursive: true });
+        const localPath = path.join(parentFolderPath, uuidv4());
         fs.writeFile(localPath, Buffer.from(data, 'base64'));
 
         fileDocument.localPath = localPath;
         const result = await filesCollection.insertOne(fileDocument);
         fileDocument.id = result.insertedId;
       }
+
       const response = {
         id: fileDocument.id,
         userId: fileDocument.userId,
@@ -68,6 +72,50 @@ class FilesController {
       };
 
       return res.status(201).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getShow(req, res) {
+    try {
+      const xToken = req.headers['x-token'];
+      const key = `auth_${xToken}`;
+      const userId = await redisClient.get(key);
+      if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+      const { id } = req.params;
+
+      if (!dbClient.DB) await dbClient.init();
+      const usersCollection = await dbClient.DB.collection('users');
+      const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+      if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+      const filesCollection = await dbClient.DB.collection('files');
+      const fileDocument = await filesCollection.findOne({ _id: ObjectId(id) });
+      const fileDocument2 = await filesCollection.findOne({ userId: ObjectId(userId) });
+      if (!fileDocument && !fileDocument2) return res.status(404).json({ error: 'Not found' });
+
+      const response = {
+        id: fileDocument.id,
+        userId: fileDocument.userId,
+        name: fileDocument.name,
+        type: fileDocument.type,
+        isPublic: fileDocument.isPublic,
+        parentId: fileDocument.parentId,
+      };
+
+      return res.status(201).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getIndex(req, res) {
+    try {
+      return res.status(200).json({ success: 'true' });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error: 'Internal Server Error' });
